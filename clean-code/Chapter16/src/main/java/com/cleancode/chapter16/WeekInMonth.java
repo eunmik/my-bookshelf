edc53a55ca18f091308f89de80ff22f8022df7dd
eunmik <eunmik1022@gmail.com>
2022-03-03 14:12:22 +0900

    Chapter 16 - [Final Version] WeekInMonth.java

--- clean-code/Chapter16/src/main/java/com/cleancode/chapter16/WeekInMonth.java
+++ clean-code/Chapter16/src/main/java/com/cleancode/chapter16/WeekInMonth.java
@@ -4,11 +4,10 @@ import java.text.DateFormatSymbols;
 
 public enum WeekInMonth {
     FIRST(1), SECOND(2), THIRD(3), FOURTH(4), LAST(0);
-    public  int index;
+    private final int index;
     WeekInMonth(int index) {
         this.index = index;
     }
-    private static DateFormatSymbols dateFormatSymbols = new DateFormatSymbols();
 
 
     public int toInt(){
