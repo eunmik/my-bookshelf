16e58fc46fecc738159817ad495a1a76451b538f
eunmik <eunmik1022@gmail.com>
2022-03-03 14:29:13 +0900

    Chapter 16 - [Final Version] DayDateFactory.java

--- clean-code/Chapter16/src/main/java/com/cleancode/chapter16/DayDateFactory.java
+++ clean-code/Chapter16/src/main/java/com/cleancode/chapter16/DayDateFactory.java
@@ -7,13 +7,9 @@ public abstract class DayDateFactory {
     }
 
     protected abstract DayDate _makeDate(int ordinal);
-
-    protected abstract DayDate _makeDate(int day, DayDate.Month month, int year);
-
+    protected abstract DayDate _makeDate(int day, Month month, int year);
     protected abstract DayDate _makeDate(int day, int month, int year);
-
     protected abstract DayDate _makeDate(java.util.Date date);
-
     protected abstract int _getMinimumYear();
     protected abstract int _getMaximumYear();
 
@@ -21,7 +17,7 @@ public abstract class DayDateFactory {
         return factory._makeDate(ordinal);
     }
 
-    public static DayDate makeDate(int day, DayDate.Month month, int year) {
+    public static DayDate makeDate(int day, Month month, int year) {
         return factory._makeDate(day, month, year);
     }
 
